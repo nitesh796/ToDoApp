59415da74498652cef083e544ce7fe7449d89756
sagar_chand <sagar_chand@excellencetechnologies.in>
2018-07-02 18:58:13 +0530

    Some modification done in the js

--- js/signup.js
+++ js/signup.js
@@ -1,5 +1,5 @@
-// var apiUrl = "http://192.168.1.121/nitesh/todoApp/";
-var apiUrl = "http://excellencetechnologies.co.in/nitesh/todoApp/";
+var apiUrl = "http://192.168.1.121/nitesh/todoApp/";
+// var apiUrl = "http://excellencetechnologies.co.in/nitesh/todoApp/";
 
 $(document).ready(function() {
   $("#btn").click(function(e){
@@ -8,45 +8,27 @@ $(document).ready(function() {
       var email = $("#email").val();
       var password = $("#password").val();
       var birthday = $("#date").val();
-
-<<<<<<< HEAD
-      if( name != "" && email != "" && password != "" && birthday != ""){
-        $.ajax({
-            url: apiUrl + 'signup.php',
-            type:'post',
-            data:{name:name, email:email, pass:password, bday:birthday},
-            dataType: 'json',
-            success:function(response){
-              console.log(response);
-            }
-        });
-
-      } else {
+      if( name != "" && email != "" && password!="" && birthday!=""){
+          $.ajax({
+              url: apiUrl + 'signup.php',
+              type:'post',
+              data:{name:name, email:email, pass:password, bday:birthday},
+              dataType: 'json',
+              success:function(response){
+                var hasData , message;
+                for (var i = 0; i < response.length; i++) {
+                  hasData = response[i].success;
+                  message = response[i].data;
+                }
+                if(hasData){
+                  $("#error-msg").html(message);
+                }
+              }
+          });
+      }
+      else {
         $("#error-msg").html("You need to fill all the field");
       }
-=======
-      if( name != "" && email != "" && password!="" && birthday!=""){
-            $.ajax({
-                url: apiUrl + 'signup.php',
-                type:'post',
-                data:{name:name, email:email, pass:password, bday:birthday},
-                dataType: 'json',
-                success:function(response){
-                  var hasData , message;
-                  for (var i = 0; i < response.length; i++) {
-                    hasData = response[i].success;
-                    message = response[i].data;
-                  }
-                  if(hasData){
-                    $("#error-msg").html(message);
-                  }
-                }
-            });
-        }
-        else {
-          $("#error-msg").html("You need to fill all the field");
-        }
->>>>>>> a24a421992fe9c801a7b90f28c60d9a49b132e6a
   });
 
 });
@@ -61,3 +43,4 @@ function validateEmail(email){
     $('#error-msg').hide();
   }
 }
+// 
