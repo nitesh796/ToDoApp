d4d9c09c939158ed4c92b5afe377e8dfffb3bf40
Aditya-Jugran <aditya@excellencetechnologies.in>
2018-07-05 13:22:15 +0530

    description added

--- details.php
+++ details.php
@@ -5,33 +5,30 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Details</title>
   <link rel="stylesheet" href="css/bootstrap.min.css">
-  <link rel="stylesheet" href="css/list.css">
+  <link rel="stylesheet" href="css/details.css">
 </head>
 <body>
   <div class="list-container-mobile">
-
     <div class="header">
       <a href="#">TODO</a>
     </div>
     <div class="contents">
       <div class="contents-header">
-        <h1>ToDo Title</h1>
+        <h4> ToDo Topic</h4>
+        <hr>
       </div>
       <div class="contents-details">
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.  </p>
       </div>
-    </div>
-
-
+    </div><!-- end of contents -->
     <div class="footer">
       <div class="savebtn">
-        <button type="button" name="button" class="btns">Save</button>
+        <button type="button" name="save_button" class="btns">Save</button>
       </div>
       <div class="deletebtn">
-        <button type="button" name="button" class="btns">Edit</button>
+        <button type="button" name="edit_btton" class="btns">Edit</button>
       </div>
-    </div>
-
+    </div> <!-- end of footer -->
   </div><!-- end list-container-mobile -->
   <script src="./js/jquery-3.3.1.min.js"></script>
   <script src="./js/bootstrap.min.js"></script>
