e6e1edd6b706192ff38b637545b33e7e70335bcd
sagar_chand <sagar_chand@excellencetechnologies.in>
2018-07-05 12:49:12 +0530

    Added the functionlity to the list page

--- js/list.js
+++ js/list.js
@@ -13,7 +13,7 @@ $(document).ready(function() {
               "<input type='checkbox'/>" +
               "<span class='box'>"+data.title+"</span>" +
               "<div class='delete-todo-item'>" +
-                "<a href='#'>" +
+                "<a href='javascript:void(0)'>" +
                   "<i class='material-icons'>delete</i>" +
                 "</a>" +
               "</div>" +
@@ -22,6 +22,60 @@ $(document).ready(function() {
           $(".todos").append(listdata);
         });
       }
-    })
+    });
 
+    $("#save_task").click(function(event) {
+      event.preventDefault();
+      var title = $("#defaultForm-email").val();
+      var description = $("#defaultForm-pass").val();
+      $.ajax({
+        url: apiUrl+'addtodo.php',
+        type: 'POST',
+        dataType: 'json',
+        data: {uid: uid , title : title , description : description},
+        success:function(response){
+          $.each(response,function(i, data) {
+            var hasData = data.success;
+            var msg = data.data;
+            var listdata = "<li id='"+data.id+"'>" +
+              "<label>" +
+                "<input type='checkbox'/>" +
+                "<span class='box'>"+data.title+"</span>" +
+                "<div class='delete-todo-item'>" +
+                  "<a href='javascript:void(0)'>" +
+                    "<i class='material-icons'>delete</i>" +
+                  "</a>" +
+                "</div>" +
+              "</label>" +
+            "</li>"
+            $(".todos").append(listdata);
+            if (hasData) {
+              $("#success-msg").html(msg).show();
+              setTimeout(function() { $("#success-msg").hide(); }, 2000);
+            }
+          });
+           $("#addTodoForm")[0].reset();
+        }
+      })
+    });
+
+    $(document).on('click', '.delete-todo-item', function () {
+      var todo_id = $(this).parentsUntil('ul')[1].id;
+      $.ajax({
+        url: apiUrl+'deletetodo.php',
+        type: 'POST',
+        dataType: 'json',
+        data: {todo_id: todo_id},
+        success:function(response){
+          var hasData;
+          for (var i = 0; i < response.length; i++) {
+            hasData = response[i].success;
+          }
+          if (hasData) {
+              $("#"+todo_id).remove();
+          }
+
+        }
+      });
+    });
 });
